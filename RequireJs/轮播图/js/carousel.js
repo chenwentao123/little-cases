5d3103c895e438bd584a989da74dd98b97dcc2bf
CWT <561198086@qq.com>
2017-12-11 18:42:34 +0800

    一些小例子

--- RequireJs/轮播图/js/carousel.js
+++ RequireJs/轮播图/js/carousel.js
@@ -19,7 +19,28 @@ define(['jquery'],function($){
      $.extend(this.defaultSettings,settings);
      this.$arrows.append.(this.$prev).append(this.$next);
      this.$container.append(this.$pic).append(this.$tab).append(this.$arrows).appendTo(this.defaultSettings.selector);
-
+     for(var i=0;i<this.defaultSettings.imgArr.length;i++){
+         this.$pic.append('<img src="'+this.defauleSettings.imgArr[i]+'"/>');
+         this.$tab.append('<li>'+(i+1)+'</li>');
+     }
+     $('img:eq(0)',this.$pic).add($('li:eq(0)', this.$tab)).addClass('selected');
+       if(this.defaultSettings.buttonStyle == 'circle'){
+           $('li', this.$nav).css({
+               borderRadius: '50%'
+           }).html('');
+       }
+       var that = this;
+       $('li', this.$tab).on('mouseover', function () {
+           that.nowIndex = $(this).index();
+           changeImg();
+       });
+       this.$prev.on('click', function () {
+           that.nowIndex--;
+           if (that.nowIndex == -1) {
+               that.nowIndex = that.defaultSettings.imgArr.length - 1;
+           }
+           changeImg();
+       });
    };
 
 });
