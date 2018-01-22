06046c9a6ba81ef1c741cad4654761768e9060b1
chenwentao123 <561198086@qq.com>
2018-01-22 16:46:19 +0800

    一些小例子

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/**
+ * Created by CWT on 2018/1/21.
+ */
+var a = 5;
+var add = function add(x, y) {
+  return x + y;
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by CWT on 2018/1/21.
+ */
+let a = 5;
+let add  = (x,y) => x + y;
