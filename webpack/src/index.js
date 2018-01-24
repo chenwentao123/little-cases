7ec3a2df57beee606c80a9a628e3989d31d68cdc
chenwentao123 <561198086@qq.com>
2018-01-24 16:37:05 +0800

    一些小例子

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,4 +1,8 @@
 import css from './css/index.css';
-import css from './css/index2.css';
+import css2 from './css/index2.css';
+import sass from './css/style.scss';
 var oDiv1 = document.querySelector('#div1');
 oDiv1.innerHTML = 'Hello My World';
+
+let b = 5;
+console.log(b);
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const glob = require('glob');
+const PurifyCSSPlugin = require('purifycss-webpack');
 module.exports = {
     entry: {
         index: './src/index.js'
@@ -18,7 +20,10 @@ module.exports = {
                 // use:['style-loader','css-loader']
                 use:ExtractTextPlugin.extract({
                     fallback:'style-loader',
-                    use:'css-loader'
+                    use:[{
+                        loader:'css-loader',
+                        options:{importloaders:1}
+                    },'postcss-loader']
                 })
             },{
                 test:/\.(png|jpg|gif)/,
@@ -32,6 +37,21 @@ module.exports = {
             },{
                 test:/\.(htm|html)$/i,
                 loader:'html-withimg-loader'
+            },{
+                test:/\.scss/,
+                // use:['style-loader','css-loader','sass-loader']
+                use:ExtractTextPlugin.extract({
+                    fallback:'style-loader',
+                    use:['css-loader','sass-loader']
+                })
+            },{
+                test:/\.(jsx|js)$/,
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                        presets: ['env', 'react']
+                    }
+                }
             }
         ]
     },
@@ -44,12 +64,20 @@ module.exports = {
             template: './src/index.html'
         }),
         new ExtractTextPlugin('css/index.css'),
-        new UglifyJsPlugin()
+        new UglifyJsPlugin(),
+        new PurifyCSSPlugin({
+            paths: glob.sync(path.join(__dirname, 'src/*.html')),
+          })
     ],
     devServer: {
         contentBase: path.resolve(__dirname, 'dist'),
         host: '127.0.0.1',
         port: '8080',
         compress: true
+    },
+    watchOptions: {
+        poll: 1000,
+        aggregateTimeout: 500,
+        ignored: /node_modules/
     }
 };
