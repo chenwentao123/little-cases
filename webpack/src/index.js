970a540ea2a36454de72ebbdd10de6090d1bf5ab
chenwentao123 <561198086@qq.com>
2018-01-25 10:54:56 +0800

    一些小例子

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,8 +1,14 @@
 import css from './css/index.css';
 import css2 from './css/index2.css';
 import sass from './css/style.scss';
+// import $ from 'jquery';
+let json = require('../author.json');
 var oDiv1 = document.querySelector('#div1');
 oDiv1.innerHTML = 'Hello My World';
 
 let b = 5;
 console.log(b);
+
+$('#div7').html('test jquery plugin');
+
+$('#div8').html(`作者：${json.name}, 年龄：${json.age}, 学校：${json.school}`);
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -4,10 +4,14 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const glob = require('glob');
 const PurifyCSSPlugin = require('purifycss-webpack');
+const webpack = require('webpack');
+const entry = require('./webpack_config/entry_webpack.js');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
-    entry: {
-        index: './src/index.js'
-    },
+    // entry: {
+    //     index: './src/index.js'
+    // },
+    entry:entry,
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
@@ -67,7 +71,20 @@ module.exports = {
         new UglifyJsPlugin(),
         new PurifyCSSPlugin({
             paths: glob.sync(path.join(__dirname, 'src/*.html')),
-          })
+          }),
+        new webpack.BannerPlugin('涛哥出世，谁与争锋！'),
+        new webpack.ProvidePlugin({
+            $:'jquery'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name:['jquery','vue'],
+            filename:'assets/js/[name].js',
+            minChunks:2
+        }),
+        new CopyWebpackPlugin([{
+            from:__dirname + '/src/public',
+            to:'./public'
+        }])
     ],
     devServer: {
         contentBase: path.resolve(__dirname, 'dist'),
