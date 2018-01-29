e127ab1e0a43e8286dc0dca54f4cd19fb697ff72
chenwentao123 <561198086@qq.com>
2018-01-29 13:26:33 +0800

    一些小例子

---
+++ vue-cli/src/components/test.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1 class="hw">Hello World</h1>
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: '这是测试组件'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hw{
+    background: #ff0000;
+}
+</style>
