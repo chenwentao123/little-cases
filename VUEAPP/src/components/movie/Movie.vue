aa9ecf1a67b4d7b6f3dcea0c700ada2986c20278
chenwentao123 <561198086@qq.com>
2018-01-31 21:39:08 +0800

    一些小例子

--- VUEAPP/src/components/movie/Movie.vue
+++ VUEAPP/src/components/movie/Movie.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="container">
       <ul class="list">
-          <li class="movieList" v-for="movie in movieList" :key="movie.id">
+          <li @click="getDetail(movie.id)" class="movieList" v-for="movie in movieList" :key="movie.id">
               <div class="image">
                   <img :src="movie.img" alt="">
               </div>
               <div class="introduction">
                   <ul>
-                      <li>{{movie.nm}}</li>
+                      <li class="name">{{movie.nm}}</li>
                       <li>{{movie.ver}}</li>
                       <li>{{movie.star}}</li>
                       <li>{{movie.showInfo}}</li>
@@ -15,10 +15,12 @@
               </div>
           </li>
       </ul>
-      <div>
+      <div class="load" v-show="isLoading">
           <img src="../../assets/img/loading.gif" alt="">
       </div>
-      <div></div>
+      <div class="end" v-show="isEnd">
+          <h2>已经到底了</h2>
+      </div>
   </div>
 </template>
 
@@ -28,26 +30,47 @@ export default {
   data () {
       return {
           movieList:[],
-          isLoading:false,
+          isLoading:true,
+          isEnd:false
       }
   },
   methods: {
         getData(){
-          axios.get(API_PROXY +  `http://m.maoyan.com/movie/list.json?type=hot&offset=${this.movieList.length}&limit=10`)
+          axios.get(API_PROXY +  `http://m.maoyan.com/movie/list.json?type=hot&offset=${
+            this.movieList.length
+            }&limit=10`)
           .then(res => {
             let list = res.data.data.movies;
+            if(list.length < 10){
+                this.isEnd = true;
+            }
+            console.log(movisList.length);
             this.movieList = this.movieList.concat(list);
+            this.isLoading = false;
             })
           .catch(res => {
                 alert('数据获取失败!')
             });
         },
-        getDetail(){
-
+        getDetail(movieId) {
+            this.$router.push(`/moviedetail/${movieId}`);
         }
   },
   created () {
       this.getData();
+  },
+  mounted() {
+    window.onscroll = () => {
+      let scrollTop = document.documentElement.scrollTop;
+      let clientHeight = document.documentElement.clientHeight;
+      let scrollHeight = document.documentElement.scrollHeight;
+      if (scrollTop + clientHeight == scrollHeight && !this.isEnd) {
+            this.isLoading = true;
+            setTimeout(() => {
+            this.getMovieData();
+            }, 1000);
+      }
+    };
   }
 }
 </script>
@@ -55,6 +78,7 @@ export default {
 <style scoped>
 .container{
     margin-top:1rem;
+    margin-bottom:2rem;
 }
 .movieList{
     display:flex;
@@ -69,6 +93,18 @@ export default {
     flex-grow: 2;
     width:0;
     padding:0 0.1rem
+}
+.name{
+    font-weight:bolder
+}
+.load{
+    text-align:center;
+    position:fixed;
+    bottom:1rem;
+    width:100%;
+}
+.end{
+    text-align:center;
 }
 </style>
 
