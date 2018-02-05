74f37edf41859bea40868180cc52a4d6d5eccb1a
chenwentao123 <561198086@qq.com>
2018-02-05 20:44:25 +0800

    一些小例子

--- VUEAPP/src/components/photo/Photo.vue
+++ VUEAPP/src/components/photo/Photo.vue
@@ -1,19 +1,42 @@
 <template>
-      <div class="container">
-          Photo
-      </div>
+    <ul class="clearfix">
+        <li class="photo" v-for="(photo,index) in photoList" :key="index">
+            <router-link :to="'/photodetail/'+index"> 
+                <img src="photo.src" alt="">
+            </router-link>
+        </li>
+    </ul>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+    data(){
+        return {
+            photoList:[]
+        }
+    },
+    created(){
+        axios.get('/static/data/photodata.json').
+        then(res => {
+            this.photoList = res.data.photoData;
+            this.$store.dispatch('setPhotoListAction',res.data.photoData);
+        }).catch(() =>{
+            alert('读取数据失败！');
+        })
+    }
 }
 </script>
 
 <style scoped>
-.container{
-    position:absolute;
-    top:1rem;
-    font-size:1.5rem;
+.photo{
+    width: 50%;
+    float: left;
+}
+
+.clearfix::after{
+    content: '';
+    display: block;
+    clear: both;
 }
 </style>
